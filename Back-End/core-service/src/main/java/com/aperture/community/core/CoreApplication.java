67c2835275e66ef9ee19f35a139c6ca94ff30aed
HALOXIAO <584714929@qq.com>
2020-09-21 14:25:03 +0800

    Core Service

---
+++ Back-End/core-service/src/main/java/com/aperture/community/core/CoreApplication.java
@@ -0,0 +1,11 @@
+package com.aperture.community.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HALOXIAO
+ * @since 2020-09-21 14:03
+ **/
+@SpringBootApplication
+public class CoreApplication {
+}
