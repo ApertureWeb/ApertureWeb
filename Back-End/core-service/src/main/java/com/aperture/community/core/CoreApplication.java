978509c705c01df2142db1e61af4c87bdd78e30a
HALOXIAO <584714929@qq.com>
2020-09-22 16:31:54 +0800

    core-service init

--- Back-End/core-service/src/main/java/com/aperture/community/core/CoreApplication.java
+++ Back-End/core-service/src/main/java/com/aperture/community/core/CoreApplication.java
@@ -1,11 +1,13 @@
 package com.aperture.community.core;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author HALOXIAO
  * @since 2020-09-21 14:03
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CoreApplication {
 }
