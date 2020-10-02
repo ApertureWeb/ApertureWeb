24439e742ac4cdc79f966428106800125b1fa959
JavaJayV <285075313@qq.com>
2020-10-02 15:30:55 +0800

    2020.10.02

--- Back-End/aperture-acl/src/main/java/com/aperture/community/acl/ApertureAclApplication.java
+++ Back-End/aperture-acl/src/main/java/com/aperture/community/acl/ApertureAclApplication.java
@@ -3,6 +3,7 @@ package com.aperture.community.acl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @Auther: JayV
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.aperture.community.acl.client")
 public class ApertureAclApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApertureAclApplication.class, args);
