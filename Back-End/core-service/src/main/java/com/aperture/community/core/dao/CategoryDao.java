bfb1c02f8f6336655f4d705e40bc94eca606bd30
JavaJayV <285075313@qq.com>
2020-10-06 16:54:14 +0800

    circle-category

--- Back-End/core-service/src/main/java/com/aperture/community/core/dao/CategoryDao.java
+++ Back-End/core-service/src/main/java/com/aperture/community/core/dao/CategoryDao.java
@@ -1,6 +1,6 @@
 package com.aperture.community.core.dao;
 
-import com.aperture.community.core.entity.CategoryEntity;
+import com.aperture.community.core.module.CmsCategoryEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -12,6 +12,6 @@ import org.apache.ibatis.annotations.Mapper;
  * @date 2020-10-06 15:46:44
  */
 @Mapper
-public interface CategoryDao extends BaseMapper<CategoryEntity> {
+public interface CategoryDao extends BaseMapper<CmsCategoryEntity> {
 	
 }
