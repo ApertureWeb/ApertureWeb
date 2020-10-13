500baddf287c3e9f4ee7712bbf38ed27d5dba9bb
HALOXIAO <584714929@qq.com>
2020-10-13 21:44:55 +0800

    nothing

--- Back-End/message-service/src/main/java/com/aperture/community/message/manager/EventRemindManager.java
+++ Back-End/message-service/src/main/java/com/aperture/community/message/manager/EventRemindManager.java
@@ -1,14 +1,7 @@
 package com.aperture.community.message.manager;
 
 import com.aperture.community.message.dao.MsEventRemindMapper;
-import com.aperture.community.message.manager.channel.EventRemindChannel;
-import com.aperture.community.message.module.converter.EventRemindConverter;
-import com.aperture.community.message.module.dto.EventRemindDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 
 /**
  * @author HALOXIAO
@@ -22,8 +15,6 @@ public class EventRemindManager {
     EventRemindManager() {
 
     }
-
-
 
 
 }
