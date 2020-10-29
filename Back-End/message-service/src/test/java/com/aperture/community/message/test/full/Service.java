d0f27c2ee8954255dfedbaecaa092872d9598da4
HALOXIAO <584714929@qq.com>
2020-10-29 16:10:10 +0800

    demo

--- Back-End/message-service/src/test/java/com/aperture/community/message/test/full/Service.java
+++ Back-End/message-service/src/test/java/com/aperture/community/message/test/full/Service.java
@@ -1,7 +1,11 @@
 package com.aperture.community.message.test.full;
 
+import com.google.common.collect.HashBiMap;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Service {
 
@@ -11,6 +15,23 @@ public class Service {
         this.vertx = vertx;
     }
 
+    public Map<Integer,String> test() {
+        Map<Integer,String> flag = new HashMap<>();
+        vertx.createSharedWorkerExecutor("asd").executeBlocking(exe -> {
+                    try {
+                        Thread.sleep(1000);
+                        flag.put(1, "1");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        return flag;
+    }
+
+
+
 }
+
 
 
