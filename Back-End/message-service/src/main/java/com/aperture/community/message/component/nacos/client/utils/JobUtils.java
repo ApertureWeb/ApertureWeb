15d4c24d8c0165a79791d97a071151e4f8697863
HALOXIAO <584714929@qq.com>
2020-11-01 22:11:30 +0800

    retry Module

--- Back-End/message-service/src/main/java/com/aperture/community/message/component/nacos/client/utils/JobUtils.java
+++ Back-End/message-service/src/main/java/com/aperture/community/message/component/nacos/client/utils/JobUtils.java
@@ -3,6 +3,7 @@ package com.aperture.community.message.component.nacos.client.utils;
 import com.aperture.community.message.component.nacos.api.WebClientFactory;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.client.WebClient;
 import org.slf4j.Logger;
@@ -19,25 +20,42 @@ public class JobUtils<R, P> {
     private Vertx vertx;
     private WebClient webClient;
     private final Logger logger = LoggerFactory.getLogger(JobUtils.class);
+    private Handler<Promise<P>> handler;
 
-    public JobUtils(Vertx vertx) {
+    public JobUtils(Vertx vertx, int times) {
         this.vertx = vertx;
         this.webClient = WebClientFactory.getWebClient();
+        this.max_attempts = times;
     }
 
-    public Future<JobUtils<R, P>> attempt(int times, Handler<P> handler) {
-        return Future.succeededFuture();
+    public Future<JobUtils<R, P>> attemptWithReturn(Handler<Promise<P>> handler) {
+        this.handler = handler;
+        return this.doLog()
+                .compose(JobUtils::attemptInternal);
 
+    }
+
+    public Future<JobUtils<R, P>> attempt(Handler<Promise<P>> handler) {
+        return null;
+    }
+
+    public Future<JobUtils<R, P>> doLog() {
+        Future<JobUtils<R, P>> future = Future.succeededFuture();
+        if (logger.isDebugEnabled()) {
+            logger.debug("retry time :" + attempts);
+        }
+        return future;
     }
 
     private Future<JobUtils<R, P>> error() {
 
+        return null;
     }
 
     private Future<JobUtils<R, P>> attemptInternal() {
         int remaining = this.max_attempts - this.attempts;
         if (remaining > 0) {
-            return this.doAttempt()
+            return null;
         } else if (remaining == 0) {
             return Future.failedFuture("attempts time done");
         } else {
@@ -46,7 +64,8 @@ public class JobUtils<R, P> {
 
     }
 
-    private Future<JobUtils<R, P>> doAttempt() {
+    private Future<Object> doAttempt() {
+        return Future.future(handler).onFailure(x -> attempts++).compose(Future::succeededFuture);
 
     }
 
